ed4782ecc7f0995685dd886a2cab687e826eae92
Jez Cope <j.cope@erambler.co.uk>
2015-07-15 21:46:44 +0100

    Take advantage of new compact resource syntax

--- default.pp
+++ default.pp
@@ -11,109 +11,31 @@ node default {
     before => Package['dropbox'],
   }
   
-  package { 'aptitude':
-    ensure => installed,
+  package {
+    ['aptitude', 'git', 'bzr', 'mercurial', 'cvs',
+     'build-essential',
+     'autoconf',
+     'ssh',
+     'gnupg2',
+     'pcscd',
+     'vim-gtk',
+     'emacs24',
+     'chromium-browser',
+     'ruby',
+     'python3',
+     'python-gpgme',
+     'dropbox',
+     'zsh',
+     'tmux',
+     'texlive',
+     'texlive-latex-extra',
+     'texlive-science',
+     'texinfo',
+     'curl',
+     'cifs-utils',
+     'winbind']:
+       ensure => installed;
   }
 
-  package { 'git':
-    ensure => installed,
-  }
-
-  package { 'bzr':
-    ensure => installed,
-  }
-
-  package { 'mercurial':
-    ensure => installed,
-  }
-
-  package { 'cvs':
-    ensure => installed,
-  }
-
-  package { 'build-essential':
-    ensure => installed,
-  }
-
-  package { 'autoconf':
-    ensure => installed,
-  }
-
-  package { 'ssh':
-    ensure => installed,
-  }
-
-  package { 'gnupg2':
-    ensure => installed,
-  }
-
-  package { 'pcscd':
-    ensure => installed,
-  }
-
-  package { 'vim-gtk':
-    ensure => installed,
-  }
-
-  package { 'emacs24':
-    ensure => installed,
-  }
-
-  package { 'chromium-browser':
-    ensure => installed,
-  }
-
-  package { 'ruby':
-    ensure => installed,
-  }
-
-  package { 'python3':
-    ensure => installed,
-  }
-
-  package { 'python-gpgme':
-    ensure => installed,
-    before => Package['dropbox'],
-  }
-
-  package { 'dropbox':
-    ensure => installed,
-  }
-
-  package { 'zsh':
-    ensure => installed,
-  }
-
-  package { 'tmux':
-    ensure => installed,
-  }
-
-  package { 'texlive':
-    ensure => installed,
-  }
-
-  package { 'texlive-latex-extra':
-    ensure => installed,
-  }
-
-  package { 'texlive-science':
-    ensure => installed,
-  }
-
-  package { 'texinfo':
-    ensure => installed,
-  }
-
-  package { 'curl':
-    ensure => installed,
-  }
-
-  package { 'cifs-utils':
-    ensure => installed,
-  }
-
-  package { 'winbind':
-    ensure => installed,
-  }
-
+  Package['python-gpgme'] -> Package['dropbox']
 }
