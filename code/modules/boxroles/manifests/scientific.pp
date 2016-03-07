efc501644b21dc62961a7b72868dd791f5fa33b7
Jez Cope <j.s.cope@sheffield.ac.uk>
2016-03-07 12:09:04 +0000

    Add public key for R packages

--- code/modules/boxroles/manifests/scientific.pp
+++ code/modules/boxroles/manifests/scientific.pp
@@ -7,6 +7,10 @@ class boxroles::scientific {
       location => 'https://mirrors.ebi.ac.uk/CRAN/bin/linux/ubuntu',
       release => "$lsbdistcodename/",
       repos => '',
+      key => {
+        id => 'E298A3A825C0D65DFD57CBB651716619E084DAB9',
+        server => 'hkp://keyserver.ubuntu.com:80',
+      },
     }
 
   }
