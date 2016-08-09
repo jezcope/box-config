1565fa0adc40de3507f237a9789cea50a0469d61
Jez Cope <j.s.cope@sheffield.ac.uk>
2016-08-09 09:08:40 +0100

    Link /usr/bin/node to /usr/bin/nodejs on Debian/Ubuntu

--- code/modules/boxroles/manifests/webdev.pp
+++ code/modules/boxroles/manifests/webdev.pp
@@ -2,9 +2,15 @@ class boxroles::webdev {
 
   $p = hiera('packages')
 
-  package { [$p['mysql'],
-             'npm',
-             'ruby-haml',
-             'hugo']: }
+  package { [$p['mysql'], 'nodejs', 'npm', 'ruby-haml', 'hugo']:
+  }
+
+  if $::osfamily == 'Debian' {
+    file { '/usr/bin/node':
+      ensure => link,
+      target => '/usr/bin/nodejs',
+      require => Package['nodejs'],
+    }
+  }
 
 }
